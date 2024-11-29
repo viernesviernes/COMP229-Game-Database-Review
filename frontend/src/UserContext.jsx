730d1ba13133f0da929ea84b7ffa230056426e40
Jumhar Viernes <jvierne4@my.centennialcollege.ca>
2024-11-29 13:12:50 -0500

    Corrected .env errors

--- frontend/src/UserContext.jsx
+++ frontend/src/UserContext.jsx
@@ -16,7 +16,7 @@ export const  UserProvider = ({ children }) => {
     try {
 
       //Request being send to backend server 
-      const response = await fetch(`${import.meta.BACKEND_URI}/login`, {
+      const response = await fetch(`${import.meta.env.VITE_BACKEND_URI}/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
@@ -37,7 +37,7 @@ export const  UserProvider = ({ children }) => {
 
   const signup = async (username, password) => {
     try {
-      const response = await fetch(`${import.meta.BACKEND_URI}/signup`, {
+      const response = await fetch(`${import.meta.env.VITE_BACKEND_URI}/signup`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
