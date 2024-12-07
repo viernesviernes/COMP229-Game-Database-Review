13ef92593bdb3c014596f89f650628f9d72289bd
KhanjanPadaliya <padaliyakhanjan@gmail.com>
2024-12-06 22:01:04 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,31 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import { useContext } from "react";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
 import Signup from "./components/Signup";
 import HomePage from "./components/HomePage";
-import Games from "./components/Games";
+import Games from "./components/Games"; 
 import GameDetails from "./components/GameDetails";
-import Latest from "./components/Latest";
+import Latest from "./components/Latest"; 
 import { ThemeProvider } from "./ColorTheme";
-import { UserProvider, UserContext } from "./UserContext";
+import { UserProvider } from "./UserContext";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function App() {
-  const { user } = useContext(UserContext);
-
   return (
     <GoogleOAuthProvider clientId="159976334384-t5n83o9qjbscgugor7kpo5j65sjldrh5.apps.googleusercontent.com">
       <ThemeProvider>
         <UserProvider>
           <Routes>
-            {/* Redirect to /home if the user is logged in */}
-            <Route path="/" element={user ? <Navigate to="/home" replace /> : <Navigate to="/login" replace />} />
-            <Route path="/login" element={user ? <Navigate to="/home" replace /> : <Login />} />
-            <Route path="/signup" element={user ? <Navigate to="/home" replace /> : <Signup />} />
-            <Route path="/profile/:username" element={user ? <Profile /> : <Navigate to="/login" replace />} />
-            <Route path="/home" element={user ? <HomePage /> : <Navigate to="/login" replace />} />
-            <Route path="/games" element={user ? <Games /> : <Navigate to="/login" replace />} />
-            <Route path="/games/:id" element={user ? <GameDetails /> : <Navigate to="/login" replace />} />
-            <Route path="/latest" element={user ? <Latest /> : <Navigate to="/login" replace />} />
+            {/* Default route redirects to the login page */}
+            <Route path="/" element={<Navigate to="/login" replace />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/profile/:username" element={<Profile />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/games" element={<Games />} />
+            <Route path="/games/:id" element={<GameDetails />} />
+            <Route path="/latest" element={<Latest />} />
+
           </Routes>
         </UserProvider>
       </ThemeProvider>
